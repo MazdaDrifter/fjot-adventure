46a77b40ac98b094d2c61f293345c023ab5f505c
asciiFeather <69516234+asciiFeather@users.noreply.github.com>
2021-03-18 12:06:31 +0800

    Update fjotadventure.cpp

--- source/npyfound/fjotadventure.cpp
+++ source/npyfound/fjotadventure.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+    std::string stdinput;
+    std::cout<<"Welcome to fjot adventures!"<<std::endl;
+    std::cout<<"Pick a fjot class:\n[Bghays][Shidneka]"<<std::endl; std::cin>>stdinput;
     return 0;
 }
